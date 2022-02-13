bf4c18845152ef578587a79042f167d9e9f28911
Si Thu Zaw <sithuzaw36@gmail.com>
2022-02-13 10:53:45 +0800

    Data translations for JSON creation

---
+++ app/src/main/java/com/team7/propertymobile/PredictionDataService.java
@@ -0,0 +1,62 @@
+package com.team7.propertymobile;
+
+import android.content.Context;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PredictionDataService {
+
+    Context context;
+
+    public static final String QUERY_FOR_PREDICTION = "http://10.0.2.2:5000/";
+
+    public PredictionDataService(Context context) {
+        this.context = context;
+    }
+
+    public interface PredictionResponseListener {
+        void onError(String message);
+
+        void onResponse(String response);
+    }
+
+    public void callPrediction (JSONArray data, PredictionDataService.PredictionResponseListener predictionResponseListener)
+    {
+        String url = QUERY_FOR_PREDICTION;
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.POST, url, data, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                String predictionResponse= response.toString();
+                predictionResponseListener.onResponse(predictionResponse);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10000,
+                2,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        ));
+
+        DataRequestSingleton.getInstance(context).addToRequestQueue(request);
+    }
+
+}
--- app/src/main/java/com/team7/propertymobile/PriceEstimatorActivity.java
+++ app/src/main/java/com/team7/propertymobile/PriceEstimatorActivity.java
@@ -7,27 +7,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class PriceEstimatorActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class PriceEstimatorActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener {
 
     List<Transaction> transactionList;
     Property selectedProperty;
 
+    String name;
     ArrayList<String> floorRange;
     ArrayList<String> floorArea;
     String district;
     String year;
     String tenure;
 
+    Spinner areaSpinner;
+    Spinner rangeSpinner;
+
+    Button predictButton;
+
+    TextView estimateView;
+
     TransactionDataService transactionDataService = new TransactionDataService(this);
+    PredictionDataService predictionDataService = new PredictionDataService(this);
 
 
     @Override
@@ -38,33 +52,18 @@ public class PriceEstimatorActivity extends AppCompatActivity implements Adapter
         Intent intent = getIntent();
         selectedProperty = (Property) intent.getSerializableExtra("Property");
 
+        name = selectedProperty.getPropertyName();
+
         loadSpinnerData();
         Toast toast = Toast.makeText(this, "oncreate", Toast.LENGTH_SHORT);
         toast.show();
 
-    }
+        predictButton = findViewById(R.id.buttonPredict);
+        predictButton.setOnClickListener(this);
 
-    private void populateSpinner() {
-        Spinner area = findViewById(R.id.spinnerArea);
-        Spinner range = findViewById(R.id.spinnerRange);
+        estimateView = findViewById(R.id.textViewEstimate);
+        estimateView.setVisibility(View.INVISIBLE);
 
-        ArrayAdapter adapterRange = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, floorRange);
-        ArrayAdapter adapterArea = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, floorArea);
-
-        adapterArea.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        adapterRange.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        TextView districtView = findViewById(R.id.textViewDistrict);
-        districtView.setText(district);
-
-        TextView tenureView = findViewById(R.id.textViewTenure);
-        tenureView.setText(tenure);
-
-        TextView yearView = findViewById(R.id.textViewYear);
-        yearView.setText(year);
-
-        area.setAdapter(adapterArea);
-        range.setAdapter(adapterRange);
     }
 
     private void loadSpinnerData() {
@@ -86,7 +85,7 @@ public class PriceEstimatorActivity extends AppCompatActivity implements Adapter
 
                     district = ref.getDistrict();
 
-                    if (ref.getTenure() == "Freehold") {
+                    if (ref.getTenure().equals("Freehold")) {
                         tenure = ref.getTenure();
                         year = "0";
                     }
@@ -95,20 +94,125 @@ public class PriceEstimatorActivity extends AppCompatActivity implements Adapter
                         int indexYear = ref.getTenure().indexOf("2");
                         String temp = ref.getTenure();
                         tenure = (String)temp.substring(0, indexTenure - 1);
-                        year =  (String) temp.substring(indexYear, temp.length());
+                        year =  (String) temp.substring(indexYear);
                     }
-                    floorRange = new ArrayList<String>();
-                    floorArea = new ArrayList<String>();
+                    floorRange = new ArrayList<>();
+                    floorArea = new ArrayList<>();
                     for (Transaction t: transactionList) {
                         if (!floorRange.contains(t.getFloorRange())){
                             floorRange.add(t.getFloorRange());
                         }
-                        if (!floorArea.contains(t.getFloorArea())){
+                        if (!floorArea.contains(String.valueOf(t.getFloorArea()))){
                             floorArea.add(String.valueOf(t.getFloorArea()));
                         }
                     }
+                    Collections.sort(floorArea);
+                    Collections.sort(floorRange);
                 }
-                populateSpinner();
+                populateData();
+            }
+        });
+    }
+
+    private void populateData() {
+
+        TextView projectName = findViewById(R.id.projectInfoTextView);
+        projectName.setText(name);
+
+        areaSpinner = findViewById(R.id.spinnerArea);
+        rangeSpinner = findViewById(R.id.spinnerRange);
+
+        ArrayAdapter<String> adapterRange = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, floorRange);
+        ArrayAdapter<String> adapterArea = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, floorArea);
+
+        adapterArea.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterRange.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        TextView districtView = findViewById(R.id.textViewDistrict);
+        districtView.setText(district);
+
+        TextView tenureView = findViewById(R.id.textViewTenure);
+        tenureView.setText(tenure);
+
+        TextView yearView = findViewById(R.id.textViewYear);
+        yearView.setText(year);
+
+        areaSpinner.setAdapter(adapterArea);
+        rangeSpinner.setAdapter(adapterRange);
+    }
+
+    private JSONArray createRequestPackage() throws JSONException {
+
+        JSONArray request = new JSONArray();
+
+        JSONObject data = new JSONObject();
+
+        LocalDate date = LocalDate.now();
+
+        String currentDate = String.valueOf(date);
+        String inputYear = currentDate.substring(0,4);
+
+        String inputMonth;
+        if (currentDate.substring(5,6).equals("0")){
+            inputMonth = currentDate.substring(6,7);
+        }
+        else {
+            inputMonth = currentDate.substring(5,7);
+        }
+
+        String floorRange = rangeSpinner.getSelectedItem().toString();
+        String floorArea = areaSpinner.getSelectedItem().toString();
+
+        data.put("projectId", String.valueOf(selectedProperty.getProjectId()));
+        data.put("district", formatDistrict());
+        data.put("floor_range", formatFloor(floorRange));
+        data.put("floor_area", floorArea.substring(0,floorArea.lastIndexOf('.')));
+        data.put("top", year);
+        data.put("tenure", tenure);
+        data.put("year", inputYear);
+        data.put("month", inputMonth);
+
+        request.put(data);
+            String debugRequest = request.toString();
+        return request;
+    }
+
+    private String formatFloor(String range) {
+        if (range.equals("-")){
+            return "1";
+        }
+        else {
+            if (range.charAt(0) == '0'){
+                String floor = range.substring(1,2);
+                return floor;
+            }
+            else {
+                String floor = range.substring(0,2);
+                return floor;
+            }
+        }
+    }
+
+    private String formatDistrict() {
+        if (district.charAt(0) == '0'){
+            return district.substring(1);
+        }
+        return district;
+    }
+
+    private void getPrediction() throws JSONException {
+
+        JSONArray data = createRequestPackage();
+        predictionDataService.callPrediction(data, new PredictionDataService.PredictionResponseListener() {
+            @Override
+            public void onError(String message) {
+
+            }
+
+            @Override
+            public void onResponse(String response) {
+                estimateView.setVisibility(View.VISIBLE);
+                estimateView.setText("Estimated price: " + response);
             }
         });
     }
@@ -116,5 +220,17 @@ public class PriceEstimatorActivity extends AppCompatActivity implements Adapter
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.buttonPredict){
+            try {
+                getPrediction();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
